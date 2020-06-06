1e50637f8172817a69f77009dc95d7123429a57d
sraza1999 <sraza1999@gmail.com>
2020-06-06 12:02:36 -0500

    Update Server with cat breeds

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1MRCwyvkCTU8jIKd6sUpFCMbvzIb_iBLM'
+export_file_name = 'cats.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['persian','siamese','shortHair']
 path = Path(__file__).parent
 
 app = Starlette()
